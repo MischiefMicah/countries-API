603d14dd3c4356f784618da5f230c597fe9bab26
MischiefMicah <Micahflanery@yahoo.com>
2023-11-18 11:20:28 -0500

    Messing around

--- countries-app/src/components/Greetings.js
+++ countries-app/src/components/Greetings.js
@@ -1,5 +1,12 @@
-function Greeting() {
-    return <h1>Hello World</h1>
+
+
+function Nav() {
+    return (
+    <div className="nav">
+        <h2>Where in the world?</h2>
+        <button><img></img>Dark Mode</button>
+    </div>
+)
 }
 
-export default Greeting
+export default Nav
