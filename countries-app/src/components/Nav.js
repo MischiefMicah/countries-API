f892ea3ea4f6c90566bf4cc20e6b98f54ac40eb0
MischiefMicah <Micahflanery@yahoo.com>
2023-11-28 06:01:36 -0500

    styled nav & added colors and font scss files

--- countries-app/src/components/Nav.js
+++ countries-app/src/components/Nav.js
@@ -1,10 +1,11 @@
 import '../styles/Nav.css'
+import moonSvg from '../styles/icons/moon-outline.svg'
 
 function Nav() {
     return (
     <div className="Nav">
         <h2>Where in the world?</h2>
-        <button>Dark Mode</button>
+        <button><img src={moonSvg} alt="Dark Mode Icon"/> Dark Mode</button>
     </div>
 )
 }
