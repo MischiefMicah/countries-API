ae6c2d609e87131e91d7ecfa71dffe70866643f7
MischiefMicah <Micahflanery@yahoo.com>
2023-12-11 17:30:36 -0500

    added desktop styles for home component

--- countries-app/src/components/Home.js
+++ countries-app/src/components/Home.js
@@ -1,12 +1,15 @@
 import Search from './Search'
 import Filter from './Filter'
 import Cards from './Cards'
+import '../styles/Home.css'
 
 function Home() {
     return (
         <div>
-            <Search />
-            <Filter />
+            <div className='home-desktop-wrap'>
+                <Search />
+                <Filter />
+            </div>
             <Cards />
         </div>
     )
--- countries-app/src/components/Search.js
+++ countries-app/src/components/Search.js
@@ -4,7 +4,7 @@ import { SearchContext } from '../contexts/SearchContext'
 
 
 function Search() {
-    const {setInputTxt, theme} = useContext(SearchContext)
+    const {inputTxt, setInputTxt, theme} = useContext(SearchContext)
 
     function searchChange(e) {
         setInputTxt(e.currentTarget.value.toLowerCase())
@@ -13,7 +13,7 @@ function Search() {
     return (
         <div className="Search">
             <label htmlFor="searchInput"></label>
-            <input className={theme === 'light' ? 'lightSearch' : 'darkSearch'} type="text" placeholder="Search for a country..." name="searchInput" maxLength="40" onChange={searchChange}></input>
+            <input className={theme === 'light' ? 'lightSearch' : 'darkSearch'} type="text" placeholder="Search for a country..." name="searchInput" maxLength="50" onChange={searchChange} value={inputTxt !== '' ? inputTxt : ''}></input>
         </div>
     )
 }
